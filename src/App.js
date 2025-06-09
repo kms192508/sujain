6048f9f2ce3d5063a5ebdbdaec75f5b2494b1cc2
kms192508 <kms767125@gmail.com>
2025-06-09 19:01:00 +0900

    0609

--- src/App.js
+++ src/App.js
@@ -12,21 +12,23 @@ import Center from './component/center'
 function App() {
   return (
     <div className='wrap'>
-      <header>
-        <h1><Link to='/'> 로고</Link></h1>
-        <nav>
-          <ul>
-            <li><Link to='/component/brand'>브랜드</Link></li>
-            <li><Link to='/component/distribute'>분양정보</Link></li>
-            <li><Link to='/component/ground'>현장정보</Link></li>
-            <li><Link to='/component/life'>라이프</Link></li>
-            <li><Link to='/component/center'>고객센터</Link></li>
+      <header className='header'>
+        <div>
+          <h1 className='h_logo'><Link to='/'> 로고</Link></h1>
+          <nav className='h_nav' style={{display: 'flex', justifyContent: 'space-between'}}>
+            <ul>
+              <li><Link to='/component/brand'>브랜드</Link></li>
+              <li><Link to='/component/distribute'>분양정보</Link></li>
+              <li><Link to='/component/ground'>현장정보</Link></li>
+              <li><Link to='/component/life'>라이프</Link></li>
+              <li><Link to='/component/center'>고객센터</Link></li>
+            </ul>
+          </nav>
+          <ul className='info_mn'>
+            <li><a href=''></a></li>
+            <li><a href=''></a></li>
           </ul>
-        </nav>
-        <ul>
-          <li><a href=''></a></li>
-          <li><a href=''></a></li>
-        </ul>
+        </div>
       </header>
         <div>
           <Route path="/" exact={true} component={Home}/>
@@ -36,7 +38,28 @@ function App() {
           <Route path="/component/life" component={Life}/>
           <Route path="/component/center" component={Center}/>
         </div>
-   
+        <footer className='footer'>
+          <h1 className='f_logo'>로고</h1>
+          <nav className='f_nav'>
+            <ul>
+              <li><a href=''>이용약관</a></li>
+              <li><a href=''>이메일무단수집거부</a></li>
+              <li><a href=''>개인정보취급방침</a></li>
+            </ul>
+            <ul>
+              <li><a href=''><img src=''/></a></li>
+              <li><a href=''><img src=''/></a></li>
+              <li><a href=''><img src=''/></a></li>
+              <li><a href=''><img src=''/></a></li>
+            </ul>
+          </nav>
+          <hr/>
+          <span>주식회사 BS한양 | 사업자 등록번호 110-81-14611 |</span>
+          <span>본사:(우)22758인천광역시 서구 청라한내로 110, 13층 4-비(청라동, 청라파이낸스센터)</span>
+          <p>서울사무소:(우)05836 서울특별시 송파구 법원로 11길 12(문정동) BS한양타워</p>
+          <p>Copyrightⓒ BS HANYANG CORPORATION Co., Ltd.All rights reserved.</p>
+
+        </footer>
     </div>
   );
 }
--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,9 +1,62 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Mainimg = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color: #bbe175;
+    h1{font-size: 20px;}
+`
+
+const Sub_com1 = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color:rgb(43, 100, 110);
+`
+
+const Sub_com2 = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color:rgb(38, 64, 114);
+    ul{
+        width: 100%;
+        height: 300px;
+        display: flex;
+        justify-content: space-between;
+    }
+    ul > li{
+        width: 30%;
+    }
+`
+
+const Sub_com3 = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color:rgb(196, 154, 18);
+`
+
+const Sub_com4 = styled.div`
+    width: 100%;
+    height: 500px;
+    background-color:rgb(141, 112, 87);
+`
 
 function Home(){
     return (
-        <div>
-            Home
+        <div className='Wrap'>
+            <Mainimg className="mainimg">mainimg</Mainimg>
+            <Sub_com1 className="sub_com1">
+                <h1>Sub_com1</h1>
+            </Sub_com1>
+            <Sub_com2 className="sub_com2">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </Sub_com2>
+            <Sub_com3 className="sub_com3">sub_com3</Sub_com3>
+            <Sub_com4 className="sub_com4">sub_com4</Sub_com4>
         </div>
     )
 }
